08fc5c822d0e997961131e2d2176ce405d0800eb
Andrey Zimin <andrey.zimin1999@gmail.com>
2024-11-26 10:07:40 +0300

    chng sett

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '62.84.122.111', 'localhost', '123.123.123.123', 'taskixddd.zapto.org'
-]
+ALLOWED_HOSTS = ['taskixddd.zapto.org']
 
 
 # Application definition
